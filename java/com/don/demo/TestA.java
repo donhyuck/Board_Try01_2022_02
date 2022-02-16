c479cfb719a39ca4e5f7508b41dc06f6bf248280
donhyuck <leedonhyuck123@gmail.com>
2022-02-16 21:04:28 +0900

    텍스트를 HTML로 응답

--- java/com/don/demo/TestA.java
+++ java/com/don/demo/TestA.java
@@ -24,15 +24,21 @@ public class TestA extends HttpServlet {
 		// 2) 한글출력을 위해서는 인코딩이 필요하다.
 		// 데이터를 주고받는 두 주체(컴퓨터간, 프로그램간..) 인코딩 방식이 같아야 한다.
 		response.setCharacterEncoding("UTF-8"); // Tomcat에게 UTF-8처리해서 보내라
-		response.setContentType("text/plain; charset=UTF-8"); // 브라우저한테 내가 보내는 걸 UTF-8로 처리해라
+		response.setContentType("text/html; charset=UTF-8"); // 브라우저한테 내가 보내는 걸 UTF-8로 처리해라
 		System.out.println("인코딩 실행");
 
 		PrintWriter out = response.getWriter();
-		out.println("hello~!");
-		out.println("my name is");
-		out.println("TestA");
+		out.println("hello~!</br>");
+		out.println("my name is</br>");
+		out.println("TestA</br>");
 
-		out.println("안녕하세요!");
+		out.println("안녕하세요!</br>");
+
+		// 3) HTML로 응답
+		// 테그로 감싸서 응답하면 해당코드를 html로 처리한다.
+		// setContentType에서 text/plain은 일반텍스트로 처리하지만 text/html로 설정하면 해당테그를 적용할 수 있다.
+		// 또한 줄바꿈은 <br> 테그로 할 수 있다. (자바 형식의 줄바꿈인 \n은 아니다.)
+		out.println("<h1>출력테스트</h1>");
 
 	}
 
